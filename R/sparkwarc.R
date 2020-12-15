ed8b8f663deea2f44dddd34e50c4489ba43f0cd4
Yitao Li <yitao@rstudio.com>
2020-12-15 21:30:02 +0000

    version 0.1.5

--- R/sparkwarc.R
+++ R/sparkwarc.R
@@ -12,13 +12,15 @@
 #'   is, should the table be cached?)
 #' @param overwrite Boolean; overwrite the table with the given name if it
 #'   already exists?
-#' @param group \code{TRUE} to group by warc segment. Currently supported
-#'   only in HDFS and uncompressed files.
-#' @param parse \code{TRUE} to parse warc into tags, attribute, value, etc.
+#' @param match_warc include only warc files mathcing this character string.
+#' @param match_line include only lines mathcing this character string.
+#' @param parser which parser implementation to use? Options are "scala"
+#'   or "r" (default).
 #' @param ... Additional arguments reserved for future use.
 #'
 #' @examples
 #'
+#' \dontrun{
 #' library(sparklyr)
 #' sc <- spark_connect(master = "spark://HOST:PORT")
 #' df <- spark_read_warc(
@@ -30,37 +32,112 @@
 #' )
 #'
 #' spark_disconnect(sc)
+#'}
 #'
-#' @export
 #' @import DBI
+#' @importFrom utils download.file
+#' @export
 spark_read_warc <- function(sc,
                             name,
                             path,
                             repartition = 0L,
                             memory = TRUE,
                             overwrite = TRUE,
-                            group = FALSE,
-                            parse = FALSE,
+                            match_warc = "",
+                            match_line = "",
+                            parser = c("r", "scala"),
                             ...) {
   if (overwrite && name %in% dbListTables(sc)) {
     dbRemoveTable(sc, name)
   }
 
-  df <- sparklyr::invoke_static(
-    sc,
-    "SparkWARC.WARC",
-    if (parse) "parse" else "load",
-    spark_context(sc),
-    path,
-    group,
-    as.integer(repartition))
+  if (!is.null(parse) && !parser %in% c("r", "scala"))
+    stop("Invalid 'parser' value, must be 'r' or 'scala'")
 
-  invoke(df, "registerTempTable", name)
+  if (is.null(parser) || parser == "r") {
+    paths_df <- data.frame(paths = strsplit(path, ",")[[1]])
+    path_repartition <- if (identical(repartition, 0L)) nrow(paths_df) else repartition
+    paths_tbl <- sdf_copy_to(
+      sc,
+      paths_df,
+      name = "sparkwarc_paths",
+      overwrite = TRUE,
+      repartition = path_repartition)
+
+    df <- spark_apply(paths_tbl, function(df) {
+      entries <- apply(df, 1, function(path) {
+        spark_apply_log("is processing warc path ", path)
+        temp_warc <- NULL
+
+        if (grepl("s3n://", path)) {
+          aws_enabled <- length(system2("which", "aws", stdout = TRUE)) > 0
+          temp_warc <- tempfile(fileext = ".warc.gz")
+
+          if (aws_enabled) {
+            spark_apply_log("is downloading warc file using aws")
+            path <- sub("s3n://", "s3://", path)
+
+            system2("aws", c("s3", "cp", path, temp_warc))
+          }
+          else {
+            spark_apply_log("is downloading warc file using download.file")
+
+            path <- sub("s3n://commoncrawl/", "https://commoncrawl.s3.amazonaws.com/", path)
+            download.file(url = path, destfile = temp_warc)
+          }
+
+          path <- temp_warc
+          spark_apply_log("finished downloading warc file")
+        }
+
+        result <- spark_rcpp_read_warc(path, match_warc, match_line)
+
+        if (!is.null(temp_warc)) unlink(temp_warc)
+
+        result
+      })
+
+      if (nrow(df) > 1) do.call("rbind", entries) else data.frame(entries)
+    }, columns = c(
+      tags = "double",
+      content = "character"
+    )) %>% spark_dataframe()
+  }
+  else {
+    if (nchar(match_warc) > 0) stop("Scala parser does not support 'match_warc'")
+
+    df <- sparklyr::invoke_static(
+      sc,
+      "SparkWARC.WARC",
+      "parse",
+      spark_context(sc),
+      path,
+      match_line,
+      as.integer(repartition))
+  }
+
+  result_tbl <- sdf_register(df, name)
 
   if (memory) {
     dbGetQuery(sc, paste("CACHE TABLE", DBI::dbQuoteIdentifier(sc, name)))
     dbGetQuery(sc, paste("SELECT count(*) FROM", DBI::dbQuoteIdentifier(sc, name)))
   }
 
-  invisible(NULL)
+  result_tbl
+}
+
+#' Reads a WARC File into using Rcpp
+#'
+#' Reads a WARC (Web ARChive) file using Rcpp.
+#'
+#' @param path The path to the file. Needs to be accessible from the cluster.
+#'   Supports the \samp{"hdfs://"}, \samp{"s3n://"} and \samp{"file://"} protocols.
+#' @param match_warc include only warc files mathcing this character string.
+#' @param match_line include only lines mathcing this character string.
+#'
+#' @useDynLib sparkwarc, .registration = TRUE
+#'
+#' @export
+spark_rcpp_read_warc <- function(path, match_warc, match_line) {
+  rcpp_read_warc(path, filter = match_warc, include = match_line)
 }
--- java/SparkWARC.scala
+++ java/SparkWARC.scala
@@ -7,53 +7,23 @@ import scala.util.matching._
 import org.apache.spark.sql.types._
 
 object WARC {
-  def load(sc: SparkContext, path: String, group: Boolean, repartitions: Int) : DataFrame = {
-    if (group) sc.hadoopConfiguration.set(
-      "textinputformat.record.delimiter", "WARC/1.0"
-    )
-
-    val warc = sc.textFile(path)
-
-    val sqlContext = new SQLContext(sc)
-    import sqlContext.implicits._
-
-    val df = warc.toDF
-    sc.hadoopConfiguration.unset("textinputformat.record.delimiter")
-
-    if (repartitions > 0) df.repartition(repartitions) else df
-  }
-
-  def parse(sc: SparkContext, path: String, group: Boolean, repartitions: Int) : DataFrame = {
+  def parse(sc: SparkContext, path: String, matchLine: String, repartitions: Int) : DataFrame = {
     val sqlContext = new SQLContext(sc)
     val warc = sc.textFile(path)
     val warcRepart = if (repartitions > 0) warc.repartition(repartitions) else warc
 
-    val warcParsed = warcRepart.flatMap(line => {
-      val tagsRegex = new Regex("<([a-zA-Z]+) ?([^>]*)>")
-
-      tagsRegex.findAllIn(line).matchData.toList.flatMap(t => {
-        val attrRegex = new Regex("[ ]*([a-zA-Z-]+)[ ]*=[ ]*\\\"([^\\\"]*)\\\".*")
-
-        List.concat(
-          List(Row.fromSeq(Seq(t.group(1), "", "", t.group(2)))),
-          attrRegex.findAllIn(t.group(2)).matchData.toList.map(a => {
-            Row.fromSeq(Seq(t.group(1), a.group(1), a.group(2), t.group(2)))
-          })
+    val warcParsed = warcRepart
+      .filter(line => line.contains(matchLine))
+      .map(line => {
+        Row(
+          "<[^>]*>".r.findAllIn(line).length,
+          line
         )
-      }).union({
-        val warcRegex = new Regex("(WARC-[a-zA-Z-]+): ?(.*)")
-
-        warcRegex.findAllIn(line).matchData.toList.map(t => {
-          Row.fromSeq(Seq(t.group(1), "", t.group(2), line))
-        })
       })
-    })
 
     val warcStruct = StructType(
-      StructField("tag", StringType, true) ::
-      StructField("attribute", StringType, true) ::
-      StructField("value", StringType, true) ::
-      StructField("original", StringType, true) :: Nil
+      StructField("tags", IntegerType, true) ::
+      StructField("content", StringType, true) :: Nil
     )
 
     sqlContext.createDataFrame(warcParsed, warcStruct)
